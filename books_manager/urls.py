0a5bb0241531e578c781c35b2bb853cdad131170
IrakliPareshishvili <i.pareshishvili@gmail.com>
2024-02-03 16:05:54 +0400

    User Authentication Setup  - added urls for register and login  - added serializers.py  - added user registration and login  - updated settings accordingly

--- books_manager/urls.py
+++ books_manager/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
+from .views import RegisterUserView, CustomAuthToken
+
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
 ]
--- books_manager/views.py
+++ books_manager/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Good</h1>')
+
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
